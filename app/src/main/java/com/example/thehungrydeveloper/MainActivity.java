c83d1e80bec283edc1d228cc9a1a310006d93920
ArturSlimac <artur.nenakhov@student.hogent.be>
2023-09-09 12:59:21 +0200

    learning about Activities and Intents

--- app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.thehungrydeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    CardView startersCard, mainsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
+
+        mainsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainCourseActivityIntent = new Intent(MainActivity.this, MainCourseActivity.class);
+                startActivity(mainCourseActivityIntent);
+            }
+        });
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startersActivityIntent = new Intent(MainActivity.this, StarterActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
+
+        TextView emailView = findViewById(R.id.text_view_email_address);
+        emailView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:testemail@m.com"));
+
+                startActivity(launchEmailAppIntent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/domain/Dish.java
@@ -0,0 +1,20 @@
+package domain;
+
+import androidx.annotation.NonNull;
+
+public class Dish {
+    private String title, description;
+    private int price;
+
+    public Dish(String title, String description, int price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return title;
+    }
+}
